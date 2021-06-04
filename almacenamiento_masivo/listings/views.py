ab9b29a6c2233468c8d548f7fdc5e64838d498e4
benjamin perez <benjamin.perez@magnet.cl>
2021-06-04 16:05:00 -0400

    feat: add base app with head template

--- almacenamiento_masivo/listings/views.py
+++ almacenamiento_masivo/listings/views.py
@@ -5,7 +5,3 @@ from .models import Listing
 class ListingListView(ListView):
     model = Listing
     template_name = 'listings/list.html'
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        return queryset.filter(city='santiago')
