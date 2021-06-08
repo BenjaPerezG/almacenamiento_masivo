de2f581c94065194ad9facbf9fd5e4beba1e2047
vicentelucero <vjlucero@miuandes.cl>
2021-06-08 14:07:14 -0400

    charts: pie, radar and bar graphics

--- almacenamiento_masivo/charts/urls.py
+++ almacenamiento_masivo/charts/urls.py
@@ -7,5 +7,15 @@ urlpatterns = [
         'listings_per_city/',
         views.ListingsPerCityView.as_view(),
         name='listings_per_city'
-    )
+    ),
+    path(
+        'availability_per_month/',
+        views.AvailabilityPerMonth.as_view(),
+        name='availability_per_month'
+    ),
+    path(
+        'room_type_per_city/',
+        views.RoomTypePerCity.as_view(),
+        name='room_type_per_city'
+    ),
 ]
--- almacenamiento_masivo/charts/views.py
+++ almacenamiento_masivo/charts/views.py
@@ -4,3 +4,9 @@ from django.views.generic import TemplateView
 
 class ListingsPerCityView(TemplateView):
     template_name = 'charts/listings_per_city.html'
+
+class AvailabilityPerMonth(TemplateView):
+    template_name = 'charts/availability_per_month.html'
+
+class RoomTypePerCity(TemplateView):
+    template_name = 'charts/room_type_per_city.html'
