4d02fbe8d47362af6fcefb50b92de98b09f0ba7c
johnny <jadonoso2@miuandes.cl>
2021-06-09 19:18:19 -0400

    semi finished

--- almacenamiento_masivo/charts/urls.py
+++ almacenamiento_masivo/charts/urls.py
@@ -9,6 +9,11 @@ urlpatterns = [
         name='listings_per_city'
     ),
     path(
+        'average_price_per_city/',
+        views.AveragePricePerCity.as_view(),
+        name='average_price_per_city'
+    ),
+    path(
         'availability_per_month/',
         views.AvailabilityPerMonth.as_view(),
         name='availability_per_month'
@@ -22,5 +27,10 @@ urlpatterns = [
         'minimum_nights_per_city/',
         views.MinimumNightsPerCity.as_view(),
         name='minimum_nights_per_city'
+    ),
+    path(
+        'occupancy_rate_per_city/',
+        views.OccupancyRatePerCity.as_view(),
+        name='occupancy_rate_per_city'
     )
 ]
