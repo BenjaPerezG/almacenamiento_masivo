3ec3a660d2702c6562e34831cb27d46bb714f0d5
benjamin perez <benjamin.perez@magnet.cl>
2021-06-09 18:47:27 -0400

    add paths to static files

--- almacenamiento_masivo/almacenamiento_masivo/settings.py
+++ almacenamiento_masivo/almacenamiento_masivo/settings.py
@@ -123,11 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-#STATIC_URL = '/static/'
-#
-#STATIC_ROOT = 'almacenamiento_masivo/static/'
-#
-#STATIC_DIR = os.path.join(BASE_DIR, 'almacenamiento_masivo/static')
+STATIC_URL = '/static/'
+
+STATIC_ROOT = 'almacenamiento_masivo/static/'
+
+STATIC_DIR = os.path.join(BASE_DIR, 'almacenamiento_masivo/static')
 
 STATICFILES_FINDERS = ['npm.finders.NpmFinder']
 
