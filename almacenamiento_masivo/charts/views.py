a1bbbe7c0a9db61bc8e7b259218959f7d954483a
benjamin perez <benjamin.perez@magnet.cl>
2021-06-08 20:31:55 -0400

    a ver si cachai vicho!

--- almacenamiento_masivo/charts/views.py
+++ almacenamiento_masivo/charts/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+import json
 
 
 class ListingsPerCityView(TemplateView):
     template_name = 'charts/listings_per_city.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # llamada a api con query y devuelta de json
+        json_dict = {
+            'santiago': 25,
+            'buenos aires': 30,
+            'bogota': 46
+        }
+        context['json'] = json.dumps(json_dict, indent=4)
+        return context
+
+
 class AvailabilityPerMonth(TemplateView):
     template_name = 'charts/availability_per_month.html'
+
 
 class RoomTypePerCity(TemplateView):
     template_name = 'charts/room_type_per_city.html'
